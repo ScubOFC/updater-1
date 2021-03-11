af9e39f809a823cbfb4c652f5886c5144fb6242e
Japrajah <70380700+Japrajah@users.noreply.github.com>
2021-03-11 21:39:10 +0700

    Update bcup.lua

--- bcup.lua
+++ bcup.lua
@@ -1,3 +1,1 @@
 --in future
-showMessage('You Pidor s 4itami, otkroi igru , zatem nazmi na eanble scripts i na no dellay/max dst')
-showMessage('Funcii 1)tpall, nazhimaesh knopku tpall  zatem  set poss')
