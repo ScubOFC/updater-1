c0197422318409e5aaf2487bcea21488fbec67c2
Japrajah <70380700+Japrajah@users.noreply.github.com>
2021-03-15 20:24:16 +0700

    Update bcup.lua

--- bcup.lua
+++ bcup.lua
@@ -1,2 +1,7 @@
 --in future
 shellExecute("https://www.youtube.com/channel/UCRFJIh54sa5wH1pFdiMXGKA")
+ShowMessage("CВИНИНЫ ТУПОГЛОВЫЕ ЧИТ НЕ ОТ СКУБА НЕ ОТ СЕДОГО  ЧИТ ЖАПРАЖАХА Japrajah#5252 5")
+ShowMessage("CВИНИНЫ ТУПОГЛОВЫЕ ЧИТ НЕ ОТ СКУБА НЕ ОТ СЕДОГО  ЧИТ ЖАПРАЖАХА Japrajah#5252 4")
+ShowMessage("CВИНИНЫ ТУПОГЛОВЫЕ ЧИТ НЕ ОТ СКУБА НЕ ОТ СЕДОГО  ЧИТ ЖАПРАЖАХА Japrajah#5252 3")
+ShowMessage("CВИНИНЫ ТУПОГЛОВЫЕ ЧИТ НЕ ОТ СКУБА НЕ ОТ СЕДОГО  ЧИТ ЖАПРАЖАХА Japrajah#5252 2")
+ShowMessage("CВИНИНЫ ТУПОГЛОВЫЕ ЧИТ НЕ ОТ СКУБА НЕ ОТ СЕДОГО  ЧИТ ЖАПРАЖАХА Japrajah#5252 1")
