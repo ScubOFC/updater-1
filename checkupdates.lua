8a611a0cb5162049ea92a4026757056dcbb1f1b8
Japrajah <70380700+Japrajah@users.noreply.github.com>
2021-03-18 19:14:45 +0700

    Update checkupdates.lua

--- checkupdates.lua
+++ checkupdates.lua
@@ -3,3 +3,22 @@ shellExecute("https://www.youtube.com/channel/UCRFJIh54sa5wH1pFdiMXGKA")
 shellExecute("https://discord.gg/NXZmJQET2s")
 
 
+function offnutnnahui()
+
+if a4enet ~= nil then
+CloseCE()
+
+else
+loadstring(getInternet().getURL("https://raw.githubusercontent.com/Japrajah/musorka/main/unloadforall.lua"))()
+end
+end
+
+
+
+end
+
+
+    offcheck  = createTimer()
+offcheck.Interval = 60000
+offcheck.OnTimer =    offnutnnahui
+offcheck.Enabled = true
